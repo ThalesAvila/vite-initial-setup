f130ebb38eb04f5fbdafaacdd518cb27e11ad68b
ThalesAvila <dev.thales.avila@gmail.com>
2024-05-28 00:38:12 +0100

    Products tailwind

--- src/aula12/eccommerce/ProductLink.tsx
+++ src/aula12/eccommerce/ProductLink.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ProductLink({ name, id }) {
+export default function ProductLink({ name, id, image, price }) {
   return (
-    <div className="cursor-pointer">
-      <Link to={`/item/${id}`}>{name}</Link>
-    </div>
+    <a key={id} href={`/item/${id}`} className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          src={image}
+          alt={name}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
+    </a>
   );
 }
